b1d5627b9b9b3654d962c4f6d9ad968a195b558d
jealvarez123 <jealvarez123@gmail.com>
2018-05-16 10:02:47 -0500

    finished what i could

--- homework_euler.py
+++ homework_euler.py
@@ -1,9 +1,13 @@
 for num in range(1,21):
     string = ""
     if num % 3 == 0:
-        string = string + "Fizz"
+
     if num % 5 == 0:
-        string = string + "Buzz"
+
     if num % 5 != 0 and num % 3 != 0:
         string = string + str(num)
     print(string)
+
+
+for num in range(1,n)
+    if
